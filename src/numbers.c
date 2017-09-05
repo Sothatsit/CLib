f85dc9891e72e8c522543a94235fabf3ed67fa30
Sothatsit <padraiglamont@gmail.com>
2017-09-05 14:10:13 +0800

    Add nextPowerOf2 methods

---
+++ src/numbers.c
@@ -0,0 +1,51 @@
+#include "numbers.h"
+
+/*
+ * Round to the nearest power of 2.
+ */
+u8 u8_findNextPowerOf2(u8 n) {
+    n--;
+    n |= n >> 1;
+    n |= n >> 2;
+    n |= n >> 4;
+    return (u8) (n + 1);
+}
+
+/*
+ * Round to the nearest power of 2.
+ */
+u16 u16_findNextPowerOf2(u16 n) {
+    n--;
+    n |= n >> 1;
+    n |= n >> 2;
+    n |= n >> 4;
+    n |= n >> 8;
+    return (u16) (n + 1);
+}
+
+/*
+ * Round to the nearest power of 2.
+ */
+u32 u32_findNextPowerOf2(u32 n) {
+    n--;
+    n |= n >> 1;
+    n |= n >> 2;
+    n |= n >> 4;
+    n |= n >> 8;
+    n |= n >> 16;
+    return n + 1;
+}
+
+/*
+ * Round to the nearest power of 2.
+ */
+u64 u64_findNextPowerOf2(u64 n) {
+    n--;
+    n |= n >> 1;
+    n |= n >> 2;
+    n |= n >> 4;
+    n |= n >> 8;
+    n |= n >> 16;
+    n |= n >> 32;
+    return n + 1;
+}
---
+++ src/numbers.h
@@ -0,0 +1,49 @@
+#ifndef JSON_types_h
+#define JSON_types_h
+
+#include <stdint.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int32_t s64;
+
+typedef float f32;
+typedef double f64;
+
+/*
+ * Get the maximum of num1 and num2.
+ */
+#define max(num1, num2) (num1 > num2 ? num1 : num2)
+
+/*
+ * Get the minimum of num1 and num2.
+ */
+#define min(num1, num2) (num1 < num2 ? num1 : num2)
+
+/*
+ * Round to the nearest power of 2.
+ */
+u8 u8_findNextPowerOf2(u8 number);
+
+/*
+ * Round to the nearest power of 2.
+ */
+u16 u16_findNextPowerOf2(u16 number);
+
+/*
+ * Round to the nearest power of 2.
+ */
+u32 u32_findNextPowerOf2(u32 number);
+
+/*
+ * Round to the nearest power of 2.
+ */
+u64 u64_findNextPowerOf2(u64 number);
+
+#endif
